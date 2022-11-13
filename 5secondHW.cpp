98381899afb4681ad8bf055525e3c30202430877
Alireza Abedi <117287926+Alirezaabedi2001@users.noreply.github.com>
2022-11-14 00:53:43 +0330

    Add files via upload

---
+++ 5secondHW.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    char n;
+    cout << "pleas inter a character: ";
+    cin >> n;
+    
+    switch (n){
+    
+	case 'i';
+		cout << "Vowel";
+		break;
+		
+	case 'a';
+		cout << "Vowel";
+		break;
+		
+	case 'e';
+		cout << "Vowel";
+		break;
+		
+	case 'o';
+		cout << "Vowel";
+		break;
+		
+	case 'u';
+		cout << "Vowel";
+		break;
+	}
+	return 0;
+}
