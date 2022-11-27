495eea67c3604ca92ac377edfdfa249bece9e5fc
Alireza Abedi <117287926+Alirezaabedi2001@users.noreply.github.com>
2022-11-28 03:23:47 +0330

    Add files via upload

---
+++ 73.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+int main() {
+	int m,n;
+	cout << "enter a number: ";
+	cin >> m;
+	
+	for(int i=1; i<=m; i++){
+	n=0;
+	   for(int j=2; j<=i; j++)
+	   n++;
+	   break
+    }
+if (n==0 && i!=1)
+cout<< i;   	   
+	    
+	return 0;
+}
